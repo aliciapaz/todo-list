c4e48ee3c2b5ac66f7a26cbfa1cdf941a52fedea
aliciapaz <aliciapazrojas@gmail.com>
2021-05-11 13:30:35 -0400

    Refactor todo factory with prototype

--- src/project.js
+++ src/project.js
@@ -1,26 +1,25 @@
 import {todoFactory} from "./todo.js";
 
+// prototype for project
+
+const projectProto = {
+  addTask(task) {
+    this.tasks.push(task)
+  },
+  deleteTask(taskTitle) {
+    const taskIndex = this.tasks.findIndex( task => task.title === taskTitle);
+    return this.tasks.splice(taskIndex, 1);
+  },
+  getTasks() {
+    return this.tasks;
+  }
+}
+
 // factory function for project
 
 const projectFactory = (title, tasks = []) => {
-
-  const addTask = task => {
-    tasks.push(task);
-}
-  const deleteTask = toDoTitle => {
-    const taskIndex = tasks.findIndex( task => task.title === toDoTitle);
-    return tasks.splice(taskIndex, 1);
-  }
-
-  const getTasks = () => tasks;
-
-  return { title, addTask, deleteTask, getTasks };
+  let obj = Object.create(projectProto);
+  obj.tasks = tasks;
+  obj.title = title;
+  return obj;
 };
-
-const myProject = projectFactory("Default");
-
- const myTodo = todoFactory('Water plants');
- console.log(myTodo)
-
- myProject.addTask(myTodo);
- console.log(myProject.getTasks());
--- src/todo.js
+++ src/todo.js
@@ -1,27 +1,31 @@
 import { format } from 'date-fns';
 
+// Todo prototype 
+const todoProto = {
+  setDescription(str) {
+    this.description = str;
+  },
+  setPriority(int) {
+    this.priority = int;
+  },
+  getDescription() {
+    return this.description;
+  },
+  getPriority() {
+    return this.priority;
+  },
+  getDate() {
+    return this.date;
+  }
+}
+
 // factory function for to-do
 const todoFactory = (title) => {
- 
-  // const setDate = () =>  handle dates
+   // const setDate = () =>  handle dates
   // const updateStatus = 
-  const setDescription = str => {
-    this.description = str;
-  }
-  const setPriority = int => {
-    this.priority = int;
-  }
-
-
-  const getDescription = () => this.description;
-  const getPriority = () => this.priority;
-  const getDate = () => this.date;
-
-  return { title, setDescription, setPriority, getDescription, getPriority, getDate };
+  let obj = Object.create(todoProto);
+  obj.title = title;
+  return obj
 };
 
-// const myTodo = todoFactory('Water plants');
-// myTodo.title = "Water my plants"
-// myTodo.setPriority(1);
-// console.log(myTodo)
 export {todoFactory};
