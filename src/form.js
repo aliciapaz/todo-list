01a7e74e797c748ff17b81f91b2a2088d948f7e0
aliciapaz <aliciapazrojas@gmail.com>
2021-05-18 00:42:37 -0400

    Fix linters

--- src/form.js
+++ src/form.js
@@ -1,46 +1,51 @@
+const create = document.createElement.bind(document);
+
 const form = () => {
-  const taskForm = document.createElement("form");
-  taskForm.className = "task-modal";
+  const taskForm = create('form');
+  taskForm.className = 'task-modal';
 
-  const inputTitle = document.createElement("input");
-  inputTitle.setAttribute("type", "text");
-  inputTitle.setAttribute("name", "title");
-  inputTitle.setAttribute("placeholder", "Task name");
+  const inputTitle = create('input');
+  inputTitle.setAttribute('type', 'text');
+  inputTitle.setAttribute('name', 'title');
+  inputTitle.setAttribute('placeholder', 'Task name');
+  inputTitle.setAttribute('maxlength', '50');
+  inputTitle.required = true;
 
-  const inputDescription = document.createElement("input");
-  inputDescription.setAttribute("type", "text");
-  inputDescription.setAttribute("name", "description");
-  inputDescription.setAttribute("placeholder", "Description");
 
-  const inputPriority = document.createElement("select");
-  inputPriority.setAttribute("name", "priority");
+  const inputDescription = create('input');
+  inputDescription.setAttribute('type', 'text');
+  inputDescription.setAttribute('name', 'description');
+  inputDescription.setAttribute('placeholder', 'Description');
 
-  const optionOne = document.createElement("option");
-  optionOne.setAttribute("value", "1");
-  optionOne.innerHTML = "High";
+  const inputPriority = create('select');
+  inputPriority.setAttribute('name', 'priority');
+
+  const optionOne = create('option');
+  optionOne.setAttribute('value', '1');
+  optionOne.innerHTML = 'High';
 
   inputPriority.appendChild(optionOne);
 
-  const optionTwo = document.createElement("option");
-  optionTwo.setAttribute("value", "2");
-  optionTwo.innerHTML = "Medium";
+  const optionTwo = create('option');
+  optionTwo.setAttribute('value', '2');
+  optionTwo.innerHTML = 'Medium';
 
   inputPriority.appendChild(optionTwo);
 
-  const optionThree = document.createElement("option");
-  optionThree.setAttribute("value", "3");
-  optionThree.innerHTML = "Low";
+  const optionThree = create('option');
+  optionThree.setAttribute('value', '3');
+  optionThree.innerHTML = 'Low';
 
   inputPriority.appendChild(optionThree);
 
-  const inputDate = document.createElement("input");
-  inputDate.setAttribute("type", "date");
-  inputDate.setAttribute("name", "date");
-  inputDate.setAttribute("value", "yyyy-mm-dd");
+  const inputDate = create('input');
+  inputDate.setAttribute('type', 'date');
+  inputDate.setAttribute('name', 'date');
+  inputDate.setAttribute('value', 'yyyy-mm-dd');
 
-  const submitBtn = document.createElement("input");
-  submitBtn.setAttribute("type", "submit");
-  submitBtn.setAttribute("value", "OK");
+  const submitBtn = create('input');
+  submitBtn.setAttribute('type', 'submit');
+  submitBtn.setAttribute('value', 'OK');
 
   taskForm.appendChild(inputTitle);
   taskForm.appendChild(inputDescription);
@@ -49,26 +54,25 @@ const form = () => {
   taskForm.appendChild(submitBtn);
 
   return taskForm;
-}
+};
 
 const newProjectForm = () => {
-  const projectForm = document.createElement("form");
-  projectForm.className = "project-form";
+  const projectForm = create('form');
+  projectForm.className = 'project-form';
 
-  const inputTitle = document.createElement("input");
-  inputTitle.setAttribute("type", "text");
-  inputTitle.setAttribute("name", "title");
-  inputTitle.setAttribute("placeholder", "Project name");
+  const inputTitle = create('input');
+  inputTitle.setAttribute('type', 'text');
+  inputTitle.setAttribute('name', 'title');
+  inputTitle.setAttribute('placeholder', 'Project name');
 
-  const submitBtn = document.createElement("input");
-  submitBtn.setAttribute("type", "submit");
-  submitBtn.setAttribute("value", "OK");
+  const submitBtn = create('input');
+  submitBtn.setAttribute('type', 'submit');
+  submitBtn.setAttribute('value', 'OK');
 
   projectForm.appendChild(inputTitle);
   projectForm.appendChild(submitBtn);
 
   return projectForm;
+};
 
-}
- 
-export { form, newProjectForm }
+export { form, newProjectForm };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import '@fortawesome/fontawesome-free/js/all';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import header from "./header";
-import panel from "./ui-projects"
+import header from './header';
+import panel from './ui-projects';
 
-let mainContainer = document.createElement("div");
-mainContainer.className = "main-container";
+const mainContainer = document.createElement('div');
+mainContainer.className = 'main-container';
 
 document.body.appendChild(mainContainer);
 mainContainer.appendChild(header());
 mainContainer.appendChild(panel());
-
