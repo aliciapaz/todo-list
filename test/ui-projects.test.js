4d002ac2470cdc2ea7e47ce586df45de299db881
aliciapaz <aliciapazrojas@gmail.com>
2021-06-07 17:48:53 -0400

    Add tests for create/remove a project from panel

--- test/ui-projects.test.js
+++ test/ui-projects.test.js
@@ -2,15 +2,18 @@
  * @jest-environment jsdom
  */
 
+import panel from "../src/ui-projects";
 
-import panel from "../src/ui-projects"
+afterEach(() => {
+  localStorage.clear();
+});
 
 test("Panel creation", () => {
   const panelTest = panel();
-   document.body.appendChild(panelTest);
-   const asideTest = document.getElementsByTagName("ASIDE");
+  document.body.appendChild(panelTest);
+  const asideTest = document.getElementsByTagName("ASIDE");
   expect(asideTest[0].className).toEqual("side-panel");
-})
+});
 
 test("Creation of task form", () => {
   const panelTest = panel();
@@ -19,22 +22,47 @@ test("Creation of task form", () => {
   buttonTask.click();
   const taskModal = document.querySelector(".task-modal");
   expect(taskModal.tagName).toBe("FORM");
-})
+});
 
-test("New project button", () =>{
+test("New project button", () => {
   const panelTest = panel();
   document.body.appendChild(panelTest);
   const newProject = document.querySelector(".new-project");
   expect(newProject.tagName).toBe("DIV");
-})
+});
 
-test("New project form creation ", () =>{
+test("New project form creation ", () => {
   const panelTest = panel();
   document.body.appendChild(panelTest);
   const newProjectButton = document.querySelector(".new-project");
   newProjectButton.click();
-  const projectForm = document.querySelector(".project-form")
+  const projectForm = document.querySelector(".project-form");
   expect(projectForm.tagName).toBe("FORM");
-})
+});
 
+test("Adds a project to the projects list", () => {
+  const panelTest = panel();
+  document.body.appendChild(panelTest);
+  const newProjectButton = document.querySelector(".new-project");
+  newProjectButton.click();
+  const projectForm = document.querySelector(".project-form");
+  projectForm.elements.title.value = "Test project";
+  projectForm.submit();
+  const projectListItem = document.querySelector(".project-li-1");
+  expect(projectListItem.innerHTML).toMatch(/Test project/);
+});
 
+test("Removes a project from the projects list", () => {
+  const panelTestNew = panel();
+  document.body.appendChild(panelTestNew);
+  const newProjectButton = document.querySelector(".new-project");
+  newProjectButton.click();
+  const projectForm = document.querySelector(".project-form");
+  projectForm.elements.title.value = "Test project new";
+  projectForm.submit();
+  const projectList = document.querySelector(".projects-ul");
+  const projectListItem = document.querySelector(".project-li-1");
+  const deleteBtn = projectListItem.childNodes[1];
+  deleteBtn.click();
+  expect(projectList.childElementCount).toBe(1);
+});
