3c77efc0c12f137411552d3a39c42d2a9fdb849e
exit <zrmartinezg@gmail.com>
2021-06-07 13:26:16 -0500

    Add tests for delete, get project from local storage

--- test/localstorage.test.js
+++ test/localstorage.test.js
@@ -1,4 +1,5 @@
 // import { expect, test } from "@jest/globals"
+import { expect } from "@jest/globals";
 import {
   getProjectsLS, updateProjectLS, addProjectLS, deleteProjectLS,
 } from "../src/localstorage"
@@ -43,3 +44,18 @@ test("add project to LS", () => {
   expect(JSON.parse(localStorage.getItem("projects"))).toEqual([{"id": 0, "title": "projectTest"}]);
 })
 
+test("get project from LS", () => {
+  const projectTest = {title:"projectTestSecond"}
+  addProjectLS(projectTest); 
+  expect(getProjectsLS()).toEqual([{"id": 0, "title": "projectTest"}, {"id": 1, "title": "projectTestSecond"}])
+})
+
+test("delete project from LS", () => {
+  const projectDelete = {"id": 1, title:"projectTestSecond"}
+  const deletePro = deleteProjectLS(projectDelete);
+  expect(getProjectsLS()).toEqual([{"id": 0, "title": "projectTest"}]) 
+})
+
+test("update project from LS", () => {
+ 
+})
