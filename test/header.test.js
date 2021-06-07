dc0d709d4eb1da81fdaa425251f7484cfe99c30b
aliciapaz <aliciapazrojas@gmail.com>
2021-06-07 15:56:55 -0400

    Add tests for header

---
+++ test/header.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import header from "../src/header"
+
+test('creates a header with a title', ()=> {
+  const myHeader = header()
+  document.body.appendChild(myHeader)
+  const headerTitle = document.getElementsByTagName('H1')
+  expect(headerTitle[0].innerHTML).toEqual('To do Organizer')
+})
