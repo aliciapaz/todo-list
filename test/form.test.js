a0892666232174cb4317cf2bd0fbe07516487b01
aliciapaz <aliciapazrojas@gmail.com>
2021-06-07 11:35:34 -0400

    Setup babel plugin for jest

--- test/form.test.js
+++ test/form.test.js
@@ -1,9 +1,7 @@
-import { test } from '@jest/globals';
 import { form, newProjectForm } from '../src/form.js';
 
 // describe("create a form", () => {
 //   const form = form();
 //   expect(form.type()).toEqual('Form');
 // })
-
-test("creat")
+//change environment to jsdom
