315d3ebb9ae3204e91ce4bb8f4fd7e5e12ee40c0
exit <zrmartinezg@gmail.com>
2021-06-07 10:19:59 -0500

    Add test folder

---
+++ test/form.test.js
@@ -0,0 +1,9 @@
+import { test } from '@jest/globals';
+import { form, newProjectForm } from '../src/form.js';
+
+// describe("create a form", () => {
+//   const form = form();
+//   expect(form.type()).toEqual('Form');
+// })
+
+test("creat")
